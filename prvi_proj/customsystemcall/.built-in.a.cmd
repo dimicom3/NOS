82d741543e173d5cbd78ae540d5dcbcf57e1c4ec
root <root@ubuntu.myguest.virtualbox.org>
2024-05-10 16:19:59 +0200

    tt

--- prvi_proj/customsystemcall/.built-in.a.cmd
+++
@@ -1,1 +0,0 @@
-cmd_customsystemcall/built-in.a := rm -f customsystemcall/built-in.a;  printf "customsystemcall/%s " customsystemcall.o | xargs ar cDPrST customsystemcall/built-in.a
--- prvi_proj/customsystemcall/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_customsystemcall/modules.order := {  :; } | awk '!x[$$0]++' - > customsystemcall/modules.order
